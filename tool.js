073c0bdad10f2beb6310392ce648a0329d21d955
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-12-05 22:23:15 +0530

    comit to master

--- tool.js
+++ tool.js
@@ -1,9 +1,13 @@
 let toolsCont = document.querySelector(".tools-cont");
 let optionsCont = document.querySelector(".options-cont");
+let optionsFlag = true;
 let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
 
-let optionsFlag = true;
+let pencilFlag = false;
+let eraserFlag = false;
 
 //true -> tools show , fasle -> hide tools
 optionsCont.addEventListener("click", (e) => {
@@ -28,3 +32,18 @@ function closeTools() {
   pencilToolCont.style.display = "none";
   eraserToolCont.style.display = "none";
 }
+pencil.addEventListener("click", (e) => {
+  //true -> show pencil tool , false -> hide pencil tool
+  pencilFlag = !pencilFlag;
+
+  if (pencilFlag) pencilToolCont.style.display = "block";
+  else pencilToolCont.style.display = "none";
+});
+
+eraser.addEventListener("click", (e) => {
+  //true -> show eraser tool , false -> hide eraser tool
+  eraserFlag = !eraserFlag;
+
+  if (eraserFlag) eraserToolCont.style.display = "flex";
+  else eraserToolCont.style.display = "none";
+});
