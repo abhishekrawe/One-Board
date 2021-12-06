abac6148ab08cdb2f9cc39f7d9d57a0befe20478
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-12-06 13:01:48 +0530

    created canvas

---
+++ canvas.js
@@ -0,0 +1,11 @@
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+//API
+let tool = canvas.getContext("2d");
+tool.beginPath(); //new graphics (path) (line)
+tool.moveTo(10,10) ; //start point
+tool.lineTo(100, 150); //end point
+tool.stroke();//fill graphics or color
