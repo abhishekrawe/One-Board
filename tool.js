4128924c92bcd044237ea63f9e6a6b2d86391b76
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-12-06 10:57:36 +0530

    chnges

--- tool.js
+++ tool.js
@@ -62,51 +62,50 @@ sticky.addEventListener("click", (e) => {
     </div>
   
   `;
-  document.body.appendChild(stickyCont);  
+  document.body.appendChild(stickyCont);
 
+  // Sticky Note Fetures
   let minimize = stickyCont.querySelector(".minimize");
   let remove = stickyCont.querySelector(".remove");
-  noteActions(minimize, remove , stickyCont);
+  noteActions(minimize, remove, stickyCont);
 
-  stickyCont.onmousedown = function(event) {
-
-   dragAndDrop(stickyCont, event);
-  
+  stickyCont.onmousedown = function (event) {
+    dragAndDrop(stickyCont, event);
   };
-  
-  stickyCont.ondragstart = function() {
+
+  stickyCont.ondragstart = function () {
     return false;
   };
-  
-})
-function noteActions(minimize , remove, stickyCont) {
-   remove.addEventListener("click", (e) => {
-       stickyCont.remove();
-   })
-   minimize.addEventListener("click", (e) => {
+});
+
+// Sticky Note Feture Minimse and remve button integration
+function noteActions(minimize, remove, stickyCont) {
+  remove.addEventListener("click", (e) => {
+    stickyCont.remove();
+  });
+  minimize.addEventListener("click", (e) => {
     let noteCont = stickyCont.querySelector(".note-cont");
-    let display= getComputedStyle(noteCont).getPropertyValue("display");
-    if(display=== "none") noteCont.style.display = "block";
-    else noteCont.style.display= "none";
-   })
+    let display = getComputedStyle(noteCont).getPropertyValue("display");
+    if (display === "none") noteCont.style.display = "block";
+    else noteCont.style.display = "none";
+  });
 }
 
-
-function dragAndDrop(element ,event) {
+// Sticky Note Drag and Drop Functions
+function dragAndDrop(element, event) {
   let shiftX = event.clientX - element.getBoundingClientRect().left;
   let shiftY = event.clientY - element.getBoundingClientRect().top;
 
-  element.style.position = 'absolute';
+  element.style.position = "absolute";
   element.style.zIndex = 1000;
- 
 
   moveAt(event.pageX, event.pageY);
 
   // moves the ball at (pageX, pageY) coordinates
   // taking initial shifts into account
   function moveAt(pageX, pageY) {
-    element.style.left = pageX - shiftX + 'px';
-    element.style.top = pageY - shiftY + 'px';
+    element.style.left = pageX - shiftX + "px";
+    element.style.top = pageY - shiftY + "px";
   }
 
   function onMouseMove(event) {
@@ -114,11 +113,11 @@ function dragAndDrop(element ,event) {
   }
 
   // move the element on mousemove
-  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener("mousemove", onMouseMove);
 
   // drop the element, remove unneeded handlers
-  element.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
+  element.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
     element.onmouseup = null;
   };
 }
