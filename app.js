d797759cd5877ccc3460a8bdc5f27c07e9d3b19e
abhishekrawe <rajputabhisheksingh53@gmail.com>
2021-12-10 21:13:48 +0530

    changes to public

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require("require"); //Access //  express is a function 
+const express = require("express"); //Access //  express is a function 
 const socket = require("socket.io");
 
 
 const app = express(); // Initialization // sever ready // calling function 
 
+app.use(express.static("public"));
 
 let port = 5000; // listeinig at port 5000
 let server = app.listen(port , () => {
     console.log ("listening to port " + port);
+})
+
+let io = socket(server);
+io.on("connection", (socket)=> {
+    console.log("Made socket connection");
 })
